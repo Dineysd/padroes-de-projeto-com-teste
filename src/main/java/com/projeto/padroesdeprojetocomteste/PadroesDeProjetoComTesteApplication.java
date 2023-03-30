7c6ff04357b38d937616a0bcff0354d25b35662d
dineysd12 <diney.sd1@gmail.com>
2023-03-30 16:05:38 -0300

    first commit

---
+++ src/main/java/com/projeto/padroesdeprojetocomteste/PadroesDeProjetoComTesteApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.padroesdeprojetocomteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesDeProjetoComTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesDeProjetoComTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/padroesdeprojetocomteste/PadroesDeProjetoComTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.padroesdeprojetocomteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesDeProjetoComTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
